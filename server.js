cd1a3a05f92fda0c8bd932481fcdd973dbc326c5
Joberman055 <jacksonoberman@gmail.com>
2023-09-15 16:01:10 -0700

    add in middleware and skeleton of controllers models and routes folders

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+//require dotenv
+require('dotenv').config()
+
+//require express
 const express = require('express')
 const app = express()
 
+//require cors
+const cors = require('cors')
+
+//post
 const PORT = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-    res.send('Homepage')
-})
+//import routes
+const routes = require('./routes/index')
+
+
+//MIDDLEWARE
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+//ROUTES
+
+app.use('/', routes)
+
+app.use((req, res) => {res.status(404).json({message: "NOT A PROPER ROUTE"})})
+
+//test route
+// app.get('/', (req, res) => {
+//     res.send('Homepage')
+// })
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}...`)
