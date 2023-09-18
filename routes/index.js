9bb056a3e9b2f0c6fa2657b3b6d8c99bebee758e
Joberman055 <jacksonoberman@gmail.com>
2023-09-17 19:10:48 -0700

    rename PokeCard.js model and set up route index

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router()
+const collectionRoute = require('./collectionRoutes')
+const pokeCardRoute = require('./pokeCardRoutes')
 
+router.use('/collections', collectionRoute)
+router.use('/collections/:collectionID/cards', pokeCardRoute)
 
 module.exports = router
